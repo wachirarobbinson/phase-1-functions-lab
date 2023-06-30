4d683d01be81a2e90e4316c909756e1f90862307
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-06-30 05:48:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;
+    return Math.abs(pickupLocation - hqLocation);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    const blocks = Math.abs(endBlock - startBlock);
+    const feetPerBlock = 264; 
+    return blocks * feetPerBlock;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        const farePrice = (distance - 400) * 0.02;
+        return farePrice;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
